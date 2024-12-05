8111e9dbfb2bd6302e554cc5cf97e6930451bf04
Ramesh-Lakmal <rameonlinej@gmail.com>
2024-12-05 11:26:15 +0530

    add boostratp and JS

---
+++ app.js
@@ -0,0 +1,24 @@
+//for(let i = 0; i<10;i++){
+    //console.log("Hello");
+//}
+
+//let numberList =[10,20,30,40,50,60,70,80,90,100];
+//
+//numberList.forEach(Number => {
+//    console.log(Number)
+//})
+//let sum = getSum(10,20);
+//console.log(sum)
+//function getSum(x,y){
+//    let sum = x+y;
+//    return sum;
+//}
+
+let customer = {
+    name:"saman",
+    age:12,
+    address:"Panadura",
+    salary:75000.00
+}
+
+console.log(customer.address);
